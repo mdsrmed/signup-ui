959dfa7b64191922457accc9aa1181e09cb499d3
mdsrmed <mdsrmed@gmail.com>
2023-09-04 17:27:18 -0400

    Initial Commit

---
+++ signup-ui/signup_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  signup_uiApp.swift
+//  signup-ui
+//
+//  Created by Md Shohidur Rahman on 9/4/23.
+//
+
+import SwiftUI
+
+@main
+struct signup_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
